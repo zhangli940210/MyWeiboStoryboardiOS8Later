f4dbd06ff827f42cf362fc5fbbdfa3d96820b8a6
gj <gj@itcast.cn>
2016-02-23 21:08:17 +0800

    初始化项目

---
+++ 15-自定义不等高的cell-storyboard有配图(iOS8之后)/15-自定义不等高的cell-storyboard有配图(iOS8之后)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  15-自定义不等高的cell-storyboard有配图(iOS8之后)
+//
+//  Created by apple on 16/2/23.
+//  Copyright © 2016年 m14a.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 15-自定义不等高的cell-storyboard有配图(iOS8之后)/15-自定义不等高的cell-storyboard有配图(iOS8之后)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  15-自定义不等高的cell-storyboard有配图(iOS8之后)
+//
+//  Created by apple on 16/2/23.
+//  Copyright © 2016年 m14a.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
